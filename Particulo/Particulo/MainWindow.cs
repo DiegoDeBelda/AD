cc5a32f0b156303ac85e95d899f55ebd3681a717
DiegoDeBelda <diegodebeldacalvo@gmail.com>
2015-10-06 10:34:20 +0200

    automatizada varias listas

--- Particulo/Particulo/MainWindow.cs
+++ Particulo/Particulo/MainWindow.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Gtk;
 using MySql.Data.MySqlClient;
 
@@ -14,29 +15,37 @@ public partial class MainWindow: Gtk.Window
 		return mysqlconection;
 	}
 
+
+
 	public ListStore rellenado(MySqlConnection a){
 
 		//sentencias--------------------------------------------------------------
 		MySqlCommand mysqlcomand =a.CreateCommand ();
-		mysqlcomand.CommandText = "select id, nombre from articulo";
+		mysqlcomand.CommandText = "select * from articulo";
 		MySqlDataReader mysqldatareader = mysqlcomand.ExecuteReader ();
 
 
 
-		TreeView.AppendColumn ("id", new CellRendererText(), "text", 0);
-		TreeView.AppendColumn ("nombre", new CellRendererText (), "text", 1);
+//		TreeView.AppendColumn ("id", new CellRendererText(), "text", 0);
+//		TreeView.AppendColumn ("nombre", new CellRendererText (), "text", 1);
+
+		String[] ColumNames = getColumnames (mysqldatareader);
+		for (int i=0; i<ColumNames.Length; i++) {
+			TreeView.AppendColumn (ColumNames [i], new CellRendererText (), "text", i);
+		}
 
 		//modelo:
 
-		ListStore listStore = new ListStore(typeof(String), typeof(String));
+//		ListStore listStore = new ListStore(typeof(String), typeof(String));
+		Type[] types = getTypes (mysqldatareader.FieldCount);
+		ListStore listStore = new ListStore(types);
 
-		String[] values = new String[2];
+	
 		while (mysqldatareader.Read()) {
-			values [0] =  mysqldatareader [0].ToString();
-			values [1] = mysqldatareader [1].ToString();
+			string[] values = getValues(mysqldatareader);
 			listStore.AppendValues (values);
-
 		}
+
 		//cerrar la conexion
 		mysqldatareader.Close ();
 		a.Close();
@@ -44,6 +53,34 @@ public partial class MainWindow: Gtk.Window
 
 		return listStore;
 
+	}
+
+	private String[] getColumnames (MySqlDataReader mysqldatareader){
+		List<string> columnames = new List<string>();
+		int count = mysqldatareader.FieldCount;
+		for (int i=0; i<count; i++) {
+			columnames.Add (mysqldatareader.GetName (i));
+		}
+		return columnames.ToArray ();
+	
+	}
+
+	private Type[] getTypes(int count){
+		List<Type> types = new List<Type> ();
+		for (int i=0; i<count; i++) {
+			types.Add (typeof(string));
+		
+		}
+		return types.ToArray ();
+	}
+
+	private string[] getValues(MySqlDataReader mysqldatareader){
+		int count = mysqldatareader.FieldCount;
+		List<string> values = new List<string> ();
+		for (int i=0; i<count; i++) {
+			values.Add (mysqldatareader [i].ToString ());
+		}
+		return values.ToArray ();
 	}
 
 
